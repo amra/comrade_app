a9b75a651eb2fecd0d57efe73c487afcae3ec6b2
Marek Hudik <hudik.marek@gmail.com>
2018-12-18 13:22:02 +0100

    sorting data

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert' as JSON;
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
@@ -41,13 +40,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    List<User> _users = DataHolder.users;
-    final _suggestions = <WordPair>[];
+//    List<User> _users = DataHolder.users;
+    final _users = DataHolder.users;
     final _biggerFont = const TextStyle(fontSize: 18.0);
 
     @override
     Widget build(BuildContext context) {
-
         return Scaffold(
             appBar: AppBar(
                 title: Text(widget.title),
@@ -64,18 +62,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     if (i.isOdd) return Divider();
 
                     final index = i ~/ 2;
-                    if (index >= _suggestions.length) {
-                        var toAdd = _users.getRange(index, index + 10).map((user) => new WordPair(user.last_name, user.first_name));
-                        _suggestions.addAll(toAdd);
-                    }
-                    return _buildRow(_suggestions[index]);
+//                    if (index >= _suggestions.length) {
+//                        var toAdd = _users.getRange(index, index + 10).map((user) => new WordPair(user.last_name, user.first_name));
+//                        _suggestions.addAll(toAdd);
+//                    }
+                    return _buildRow(_users[index]);
                 });
     }
 
-    Widget _buildRow(WordPair pair) {
+    Widget _buildRow(User user) {
         return ListTile(
             title: Text(
-                pair.asPascalCase,
+                user.last_name + ' ' + user.first_name,
                 style: _biggerFont,
             ),
         );
@@ -94,6 +92,12 @@ Future<List<User>> loadData() async {
         Data data = Data.fromJson(json);
         print("data: " + data.colleges.length.toString());
 
+        data.colleges.sort((user1, user2) {
+            if (user1.last_name == user2.last_name) {
+                return user1.first_name.compareTo(user2.first_name);
+            }
+            return user1.last_name.compareTo(user2.last_name);
+        });
         return data.colleges;
     } catch (e) {
         print(e);
